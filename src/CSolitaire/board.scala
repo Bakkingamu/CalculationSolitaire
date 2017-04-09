abbe04009de13b2c2f3a31f27ff9f8cf7e56a99b
Alex Googe <alexgoogethera@gmail.com>
2017-04-09 14:44:43 -0400

    Added images to the board.

--- src/CSolitaire/board.scala
+++ src/CSolitaire/board.scala
@@ -26,11 +26,25 @@ class board extends Application {
     val waste3 = new ImageView
     val waste4 = new ImageView
     //test card
-    val c : Card = new Card(SUIT.CLUB, 7)
+    val c : Card = new Card(SUIT.CLUB, 1)
+    val c1 : Card = new Card(SUIT.DIAMOND, 2)
+    val c2 : Card = new Card(SUIT.HEART, 3)
+    val c3 : Card = new Card(SUIT.SPADE, 4)
     //set deck pile to test
     //gui
     primaryStage.setTitle("Calculation Solitaire")
+    val img1 : Image = new Image("CSolitaire/resources/backr.png")
+    deck.setImage(img1)
+    val img2 : Image = new Image("CSolitaire/resources/border.png")
+    discard.setImage(img2)
     found1.setImage(c.img)
+    found2.setImage(c1.img)
+    found3.setImage(c2.img)
+    found4.setImage(c3.img)
+    waste1.setImage(img2)
+    waste2.setImage(img2)
+    waste3.setImage(img2)
+    waste4.setImage(img2)
     val root = new GridPane
     root.setPadding(new Insets(10))
     root.setVgap(10)
