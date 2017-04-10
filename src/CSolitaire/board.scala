4ad22e0c3abe9cd1514dae598ad67812558dcd10
Bakkingamu <bakkingamu0@gmail.com>
2017-04-09 20:00:13 -0400

    updated board

--- src/CSolitaire/board.scala
+++ src/CSolitaire/board.scala
@@ -1,8 +1,8 @@
 package CSolitaire
-
 /**
   * Created by Justin on 3/19/2017.
   */
+
 import CSolitaire._
 import javafx.application.Application
 import javafx.geometry.Insets
@@ -11,9 +11,12 @@ import javafx.scene.image.{Image, ImageView}
 import javafx.scene.layout._
 import javafx.scene.paint.Color
 import javafx.stage._
-import javax.smartcardio.Card
+import javafx.scene.text.Text
+import javafx.event.EventHandler
+import javafx.scene.input.{ClipboardContent, Dragboard, MouseEvent, TransferMode}
+
 class board extends Application {
-  override def start(primaryStage: Stage ){
+  override def start(primaryStage: Stage ) {
     //slot for each card on the board
     val deck = new ImageView
     val discard = new ImageView
@@ -26,34 +29,58 @@ class board extends Application {
     val waste3 = new ImageView
     val waste4 = new ImageView
     //test card
-    val c : Card = new Card(SUIT.CLUB, 1)
-    val c1 : Card = new Card(SUIT.DIAMOND, 2)
-    val c2 : Card = new Card(SUIT.HEART, 3)
-    val c3 : Card = new Card(SUIT.SPADE, 4)
+    val c: Card = new Card(SUIT.CLUB, 1)
+    val c1: Card = new Card(SUIT.DIAMOND, 2)
+    val c2: Card = new Card(SUIT.HEART, 3)
+    val c3: Card = new Card(SUIT.SPADE, 4)
     //set deck pile to test
     //gui
+
     primaryStage.setTitle("Calculation Solitaire")
-    val img1 : Image = new Image("CSolitaire/resources/backr.png")
-    deck.setImage(img1)
-    val img2 : Image = new Image("CSolitaire/resources/border.png")
-    discard.setImage(img2)
+    val back : Image = new Image("CSolitaire/resources/backr.png")
+    deck.setImage(back)
+    val border : Image = new Image("CSolitaire/resources/border.png")
+    var currentImg : Image = null
+    discard.setImage(border)
     found1.setImage(c.img)
     found2.setImage(c1.img)
     found3.setImage(c2.img)
     found4.setImage(c3.img)
-    waste1.setImage(img2)
-    waste2.setImage(img2)
-    waste3.setImage(img2)
-    waste4.setImage(img2)
+    waste1.setImage(border)
+    waste2.setImage(border)
+    waste3.setImage(border)
+    waste4.setImage(border)
+    var click : EventHandler[MouseEvent] = new EventHandler[MouseEvent]() {
+      def handle(event: MouseEvent): Unit = {
+        if (currentImg == null)
+          {
+              currentImg = event.getSource().asInstanceOf[ImageView].getImage()
+              event.getSource().asInstanceOf[ImageView].setImage(border)
+          }else{
+          event.getSource().asInstanceOf[ImageView].setImage(currentImg)
+          currentImg = null
+        }
+
+      }
+    }
+    found1.setOnMouseClicked(click)
+    found2.setOnMouseClicked(click)
+    found3.setOnMouseClicked(click)
+    found4.setOnMouseClicked(click)
+    waste1.setOnMouseClicked(click)
+    waste2.setOnMouseClicked(click)
+    waste3.setOnMouseClicked(click)
+    waste4.setOnMouseClicked(click)
+
     val root = new GridPane
+
     root.setPadding(new Insets(10))
     root.setVgap(10)
     root.setHgap(10)
     root.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)))
     //add all piles to grid
-    root.add(deck, 1,0)
+    root.add(deck, 1, 0)
     root.add(discard, 2, 0)
-
     root.add(found1, 3, 0)
     root.add(found2, 4, 0)
     root.add(found3, 5, 0)
@@ -63,12 +90,10 @@ class board extends Application {
     root.add(waste3, 5, 1)
     root.add(waste4, 6, 1)
     //gui
-    val scene= new Scene(root)
+    val scene = new Scene(root)
     primaryStage.setScene(scene)
     primaryStage.show()
   }
-  //loads image from card
-
 }
 //main
 object run{
